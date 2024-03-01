c766da7b2ab29c238c7f26079868abf48442735e
Khai Dong <dongk@union.edu>
2024-03-01 11:30:00 -0500

    some basic behavior for avoid

--- src/avoid.py
+++ src/avoid.py
@@ -13,6 +13,7 @@
 #               moreover, I slow the robot down as it is approaching the desired location
 
 
+
 import rospy, math
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
@@ -33,9 +34,11 @@ class AvoidObstTurtleBot:
         self.distance_at_angle = [0] * 360
         self.roll, self.pitch, self.yaw = None, None, None
         self.front_dis = None
+        self.cur_twist = None
+        self.heading = None
+
         self.init_odom = False
         self.init_laser = False
-        self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
     def odom_update(self, odom_read):
         self.x, self.y = odom_read.pose.pose.position.x, odom_read.pose.pose.position.y
@@ -52,58 +55,19 @@ class AvoidObstTurtleBot:
                              for angle in self.front_dis_ang_samples)
         self.init_laser = True
 
-    def mv_forward(self, lin_spd):
-        forward_msg = Twist()
-        forward_msg.linear.x = lin_spd
-        self.pub.publish(forward_msg)
+    def twist_update(self, twist_msg):
+        self.cur_twist = twist_msg
 
-    def stop(self):
-        self.pub.publish(Twist())
+    def heading_update(self, heading_msg):
+        self.heading = heading_msg
 
-    def rotate(self, angle, max_ang_spd, isClockwise, tolerance=math.radians(0.1)):
-        # use odometry data to figure out the current angle instead of timing
-        outData = Twist()   
-        initial_degree = self.yaw
-        target_degree = self.yaw + (-1)**isClockwise * angle
-        if target_degree < 0:
-            target_degree += 2 * math.pi
+    def create_adjusted_twist(self, max_lin_speed=0.3, max_ang_spd=math.radians(20)):
+        out_msg = Twist()
 
-        min_spd = tolerance / 0.1 # magic number, assuming the robot can stop within 0.1 second
+        if self.heading is None: # spin around if no heading is given
+            out_msg.angular.z = max_ang_spd
+            # possibly found a heading if keep turning around
 
-        rate = rospy.Rate(30)
-        while not rospy.is_shutdown():
-            cur_degree = self.yaw
-            if cur_degree < 0:
-                cur_degree += 2 * math.pi
-            if  abs(cur_degree - target_degree) <= tolerance:
-                break
-            outData.angular.z = (-1)**isClockwise * max_ang_spd \
-                        * math.sqrt(abs(cur_degree - target_degree) / abs(initial_degree - target_degree))
-            if abs(outData.angular.z) < min_spd:
-                outData.angular.z = outData.angular.z / abs(outData.angular.z) * min_spd
-            self.pub.publish(outData)
-            rate.sleep()
-        # print()
-        self.pub.publish(Twist())   
-
-    def angle_with_most_free_space(self):
-        # make a copy before calculation to avoid changing
-        lidar_reading = self.distance_at_angle.copy()
-        res_angle, max_dist = -1, 0
-        #  take of 45 degree positions
-        angles = range(0, 360, 45)
-        for angle in angles:
-            dist_at_angle = min(lidar_reading[(angle + a + 360) % 360] * math.cos(math.radians(abs(a))) 
-                                 for a in self.front_dis_ang_samples)    
-            print("Angle, Distance: ", angle, dist_at_angle)
-            print([lidar_reading[(angle + a + 360) % 360] * math.cos(math.radians(abs(a))) for a in self.front_dis_ang_samples])
-
-            if dist_at_angle > max_dist:
-                max_dist = dist_at_angle
-                res_angle = angle
-        print()
-
-        return res_angle
 
 if __name__ == '__main__':
 
@@ -116,8 +80,15 @@ if __name__ == '__main__':
     # setup
     rob = AvoidObstTurtleBot(diameter, distance_tolerance)
     rospy.init_node('avoid_obstacles', anonymous=True)
+
+
+
     rospy.Subscriber("/odom", Odometry, rob.odom_update)
-    rospy.Subscriber("/scan", LaserScan, rob.laser_update)    
+    rospy.Subscriber("/scan", LaserScan, rob.laser_update)   
+
+    rospy.Subscriber("/cmd_vel", Twist, rob.twist_update) # to slowly ease out from current speed for smoother   
+    rospy.Subscriber("/heading", Odometry, rob.heading_update) # to know the desired
+
     rate = rospy.Rate(10)
 
     # wait for odometer and lidar to response
@@ -126,23 +97,11 @@ if __name__ == '__main__':
         rate.sleep()
     print("Sensors responded")
 
-    # for angle in range(0, 360, 45):
-    #     input(f"{angle}")
-    #     rob.rotate(math.radians(angle), angular_speed, True)
-
+    
+    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
     while not rospy.is_shutdown():
-        front_dis = rob.front_dis
-        if front_dis > rob.distance_tolerance:
-            print("No ostacles ahead, move forward")
-            rob.mv_forward(linear_spd)
-        else:
-            rob.stop()
-            # turn to the angle with the most free space
-            angle = rob.angle_with_most_free_space()
-            print("Ostacles ahead, turn to angle with most space")
-            print("angle w/ most free space: ", angle)
-            rob.rotate(math.radians(angle), angular_speed, True)
-            
+        msg = rob.create_adjusted_twist()
+        pub.publish(msg)
         rate.sleep()
 
     rospy.spin()
